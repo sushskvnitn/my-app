2719c8aa65fc2d2d244ac678333845a309e2731d
Sushant Kogurwar <88105162+sushskvnitn@users.noreply.github.com>
2021-10-16 19:06:02 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Navbar(props) {
     return (
 <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} fixed-top`}>
-  <div className="container-fluid">
+  <div className="container fixed-top">
     <a className="navbar-brand" href="#">{props.title} </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
